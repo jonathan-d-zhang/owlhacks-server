69f6c94b3714f859f733a5cce6df3da90fe9711a
jz9 <69145546+jonathan-d-zhang@users.noreply.github.com>
2023-02-11 11:01:47 -0500

    Upload files template

--- main.py
+++ main.py
@@ -1,8 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
-
-@app.get("/")
-async def root():
+@app.post("/open")
+async def open_room():
     return {"message": "Hello World"}
+
+
+@app.post("/stt")
+async def convert_speech(data: UploadFile):
+    return {"filename": data.filename}
